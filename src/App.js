1b7416c351e7d2e93d02996a3f037ca21cff9b54
IneMaroc <ine.marocchi@gmail.com>
2021-06-09 23:44:21 -0300

    instalacion sass

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Productos</h1>
-        <ul>
-          <li>Remera </li>
-          <li>Pantalon</li>
-          <li>Pollera</li>
-        </ul>
+    
     </div>
   );
 }
